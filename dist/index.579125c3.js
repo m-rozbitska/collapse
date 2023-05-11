c74c40ce84089b54239d7750d54ac698c376cc6a
Mariia Rozbitska <melissa180lily@gmail.com>
2023-05-11 16:43:50 +0300

    feat: add js code

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -1,2 +1,15 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.querySelectorAll(".collapse__title").forEach((el)=>{
+        el.addEventListener("click", ()=>{
+            const content = el.nextElementSibling;
+            console.log(content);
+            if (content.style.maxHeight) document.querySelectorAll(".collapse__content").forEach((el)=>el.style.maxHeight = null);
+            else {
+                document.querySelectorAll(".collapse__content").forEach((el)=>el.style.maxHeight = null);
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    });
+});
 
 //# sourceMappingURL=index.579125c3.js.map
